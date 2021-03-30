b00c543f147d5b23ddeb1386460be2b763b2b8e9
rahmatsuryamunajar <12050112380@students.uin-suska.ac.id>
2021-03-30 11:31:01 +0700

    project if else statement

---
+++ src/ConditionalAndLoops/IfElseStatements.java
@@ -0,0 +1,13 @@
+package ConditionalAndLoops;
+
+public class IfElseStatements {
+    public static void main(String[] args) {
+        int x = 10; int y = 5;
+        if (x > y){
+            System.out.println(x);
+        }
+        else {
+            System.out.println(y);
+        }
+    }
+}
---
+++ src/ConditionalAndLoops/NestedIfStatement.java
@@ -0,0 +1,12 @@
+package ConditionalAndLoops;
+
+public class NestedIfStatement {
+    public static void main(String[] args) {
+        int x = 37;
+        if (x > 22) {
+            if (x > 31) {
+                System.out.println("it works!");
+            }
+        }
+    }
+}
